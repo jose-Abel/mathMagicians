5d5b25464c1389ebe509c9a291742735044ded7a
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-11 11:10:27 -0400

    Solution for the hackerrank diagonal difference problem

---
+++ diagonalDifference.js
@@ -0,0 +1,23 @@
+function diagonalDifference(arr) {
+    // Write your code here
+    let firstSum = 0;
+    let secondSum = 0;
+    let k = arr.length - 1;
+    for(let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (i === j) {
+                firstSum += arr[i][j];
+            }
+            if (j === k) {
+            secondSum += arr[i][j];
+            }
+            
+        }
+        k--;
+        if (k < 0) {
+            k = 2;
+        }
+    }
+    return Math.abs(firstSum - secondSum);
+
+}
